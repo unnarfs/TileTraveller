b204204ba64031e577e16188ad82221e980fc217
Unnar Fridrik Sigurdsson <unnar18@ru.is>
2018-09-18 17:53:17 +0000

    First commit

---
+++ TileTraveller.py
@@ -0,0 +1,13 @@
+"""
+Taka punkt sem byrjar á hnitum x, y = 1 og færa hann í gegnum völundarhús. Hann verður að vera innan 1 < x, y < 3.
+Eftirfarandi hreyfingar eru ekki leyfðar (veggir):
+1,1 -> má bara fara norður
+2,2 -> má bara fara suður eða vestur
+2,1 -> má bara fara norður
+2,3 -> má bara fara vestur eða austur
+3,2 -> má bara fara norður eða suður
+"""
+
+#Breytur fyrir hreyfingu leikmannsins í völundarhúsinu
+x = 1
+x = 1
