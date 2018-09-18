1e9884c98d9a32e45e15c72b9f4a1f951e440b77
Unnar Fridrik Sigurdsson <unnar18@ru.is>
2018-09-18 18:16:50 +0000

    Bætti við smá lykkju fyrir fyrsta reit

--- TileTraveller.py
+++ TileTraveller.py
@@ -11,8 +11,19 @@ Eftirfarandi hreyfingar eru ekki leyfðar (veggir):
 #Breytur fyrir hreyfingu leikmannsins í völundarhúsinu
 x = 1
 x = 1
+#Strengur fyrir innslátt áttar sem ferðast á í
+str_movement = ""
 
 #while lykkja sem keyrir þangað til að x = 3 og y = 1
 
 while x != 3 and y != 1:
+    if X == 1 and y == 1:
+        print("You can travel: (N)orth")
+        str_movement = input("Direction: ")
+        str_movement = str_movement.lower()
+        while str_movement != n:
+            print("Not a valid direction!")
+            str_movement = input("Direction: ")
+            str_movement = str_movement.lower()
+        y += 1
     
